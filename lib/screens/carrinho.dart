5e93d7f040a58211dd30da600039034de1e79695
DevGomes78 <gomes.amilson@gmail.com>
2022-02-11 18:01:46 -0300

    extraido metodo container

--- lib/screens/carrinho.dart
+++ lib/screens/carrinho.dart
@@ -22,121 +22,7 @@ class Carrinho extends StatelessWidget {
         title: Text('Carrinho', style: AppTextStyle.text4Font24),
         centerTitle: true,
       ),
-      body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          child: ListView.builder(
-              itemCount: listaCompra.length,
-              itemBuilder: (context, index) {
-                teste = (botton.counter.toDouble() *
-                    listaCompra[index].valor!.toDouble());
-                return Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Card(
-                    elevation: 5,
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.symmetric(vertical: 0.0),
-                                  height: 70,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10),
-                                    child: Container(
-                                        height: 80,
-                                        width: 80,
-                                        child: Image.network((listaCompra[index]
-                                            .img
-                                            .toString()))),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              width: 150,
-                            ),
-                            Column(
-                              children: [
-                                Text(
-                                  listaCompra[index].nome.toString(),
-                                  style: AppTextStyle.text1Font22,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10.0, vertical: 10),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Card(
-                          elevation: 5,
-                          child: Container(
-                            height: 50,
-                            color: Color(0xffB0D2E7FF),
-                            width: double.infinity,
-                            child: Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () {
-                                    botton.decrement();
-                                    teste = (botton.counter.toDouble() *
-                                        listaCompra[index].valor!.toDouble());
-                                  },
-                                  icon: Icon(Icons.remove),
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    botton.increment();
-                                    teste = (botton.counter.toDouble() *
-                                        listaCompra[index].valor!);
-                                  },
-                                  icon: Icon(Icons.add),
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'x',
-                                  style: AppTextStyle.text5Font18,
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  botton.counter.toString(),
-                                  style: AppTextStyle.text2Font20,
-                                ),
-                                SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  'Total',
-                                  style: AppTextStyle.text5Font18,
-                                ),
-                                SizedBox(
-                                  width: 30,
-                                ),
-                                Text(
-                                  NumberFormat('R\$ #.00', 'pt-BR')
-                                      .format(teste),
-                                  style: AppTextStyle.text2Font20,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              })),
+      body: buildContainer(listaCompra, botton),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: botton.currentIndex,
         onTap: (index) {
@@ -162,6 +48,127 @@ class Carrinho extends StatelessWidget {
             title: Text('Conta'),
           ),
         ],
+      ),
+    );
+  }
+
+  buildContainer(
+    List<Produto> listaCompra,
+    ProdutoProvider botton,
+  ) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: ListView.builder(
+        itemCount: listaCompra.length,
+        itemBuilder: (context, index) {
+          teste = (botton.counter.toDouble() *
+              listaCompra[index].valor!.toDouble());
+          return Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Card(
+              elevation: 5,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 0.0),
+                            height: 70,
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: Container(
+                                  height: 80,
+                                  width: 80,
+                                  child: Image.network(
+                                      (listaCompra[index].img.toString()))),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 150,
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            listaCompra[index].nome.toString(),
+                            style: AppTextStyle.text1Font22,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 10),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Card(
+                    elevation: 5,
+                    child: Container(
+                      height: 50,
+                      color: Color(0xffB0D2E7FF),
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              botton.decrement();
+                              teste = (botton.counter.toDouble() *
+                                  listaCompra[index].valor!.toDouble());
+                            },
+                            icon: Icon(Icons.remove),
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              botton.increment();
+                              teste = (botton.counter.toDouble() *
+                                  listaCompra[index].valor!);
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'x',
+                            style: AppTextStyle.text5Font18,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            botton.counter.toString(),
+                            style: AppTextStyle.text2Font20,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'Total',
+                            style: AppTextStyle.text5Font18,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            NumberFormat('R\$ #.00', 'pt-BR').format(teste),
+                            style: AppTextStyle.text2Font20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
